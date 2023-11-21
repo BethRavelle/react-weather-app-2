b92ab05e3d6d776f7c2c81adf96cec589e79bc80
BethRavelle <143979967+BethRavelle@users.noreply.github.com>
2023-11-21 11:11:06 +1000

    add Github link

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import React from "react";
+
 import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Beth Ravelle and is
+        <a
+          href="https://github.com/BethRavelle/react-weather-app-2"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
+}
