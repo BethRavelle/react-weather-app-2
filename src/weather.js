ec8ea91f346c54dd1a4913ffe0217b5d6973f10b
BethRavelle <143979967+BethRavelle@users.noreply.github.com>
2023-11-24 17:49:42 +1000

    icon

--- src/weather.js
+++ src/weather.js
@@ -16,8 +16,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       description: response.data.condition.description,
       date: new Date(response.data.time * 1000),
-      iconUrl:
-        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png",
+      iconUrl: response.data.condition.icon_url,
     });
   }
 
