4cae607a0e43940f7b302438593fd658e24895d5
BethRavelle <143979967+BethRavelle@users.noreply.github.com>
2023-11-25 16:53:17 +1000

    removed h1 and a underlines

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather defaultCity="New York" />
         <footer>
           This project was coded by Beth Ravelle and is
