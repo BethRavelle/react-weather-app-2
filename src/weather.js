eb83bf79956fe2f9e08042a1b7b2feb2a08233dd
BethRavelle <143979967+BethRavelle@users.noreply.github.com>
2023-11-25 15:21:19 +1000

    icon updates

--- src/weather.js
+++ src/weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       description: response.data.condition.description,
       date: new Date(response.data.time * 1000),
-      iconUrl: response.data.condition.icon_url,
+      icon: response.data.condition.icon,
     });
   }
 
